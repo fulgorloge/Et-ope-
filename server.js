9f0c36480bead3f2bf93fac93abc68a7d20f08de
fulgorloge <santiago0160colombia@gmail.com>
2025-07-28 07:27:17 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+/**
+ * @file server.js
+ * @description Lógica del servidor para el juego de cartas multijugador usando Node.js y Socket.IO.
+ * @author [Tu Nombre o Nombre de tu Compañía]
+ * @copyright © 2024 [Tu Nombre o Nombre de tu Compañía]. Todos los derechos reservados.
+ */
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+// ... (resto de tu código server.js) ...
