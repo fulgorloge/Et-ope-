762e882e388da9f85a5bb16dcc7a1be70c544439
fulgorloge <santiago0160colombia@gmail.com>
2025-07-31 12:37:33 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
-
+const port = process.env.PORT || 4000
 const app = express();
 const server = http.createServer(app);
 
